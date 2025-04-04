79426eb838a1f53756e26c2301b5373e9b2fe3e0
well-nice <phil@wellnice.com>
2025-04-04 15:07:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Mount API routes
 app.use('/api', conciergeRoutes);
+
+// Greeting endpoint for the concierge
+app.get('/api/greeting', (req, res) => {
+  const hour = new Date().getHours();
+  let greeting = "Good day";
+  
+  if (hour < 12) greeting = "Good morning";
+  else if (hour < 18) greeting = "Good afternoon";
+  else greeting = "Good evening";
+  
+  res.json({ 
+    greeting: `${greeting}. Tell me what you're after and I'll go find it.` 
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Well Nice Concierge is running beautifully.');
