aa0193d2f17533461687386555ebb0d74be57e98
well-nice <phil@wellnice.com>
2025-04-04 16:22:49 +0100

    Update conciergeRoutes.js

--- routes/conciergeRoutes.js
+++ routes/conciergeRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getConciergeResponse, getGreeting } = require('../controllers/conciergeController');
+const { 
+  getConciergeResponse, 
+  getGreeting, 
+  getInsightsSummary,
+  generatePeriodicInsights
+} = require('../controllers/conciergeController');
 
 // Main concierge endpoint
 router.post('/concierge', getConciergeResponse);
 
 // Greeting endpoint
 router.get('/greeting', getGreeting);
+
+// Insights-related endpoints
+router.get('/insights', getInsightsSummary);
+router.post('/generate-insights', generatePeriodicInsights);
 
 module.exports = router;
